f69102c6d75628b5204fbd28174e259cf70dd7b7
LoisKOUNINEF <lkouninef@gmail.com>
2022-06-09 22:20:28 +0200

    better looking views

--- lib/views/item_view.rb
+++ lib/views/item_view.rb
@@ -1,9 +1,11 @@
 class View
   def create_item
+    print "-" * 50
     get_name
     get_price
     get_quantity
     get_brand
+    print "-" * 50
 
     return params = { name: @name, price: @price, quantity: @quantity, brand: @brand }
   end
@@ -11,10 +13,10 @@ class View
   def get_name
     @name
     loop do
-      puts "What's the item name ?\n>"
+      print "\nWhat's the item name ?\n>"
       @name = gets.chomp
       break if @name.length > 0 && @name.length <= 100
-      print "Name's should be between 1 and 100 characters. Please try again !"
+      print "\nName's should be between 1 and 100 characters. Please try again !"
     end
     return @name
   end
@@ -22,10 +24,10 @@ class View
   def get_price
     @price
     loop do
-      puts "What's the item price ?\n>"
+      print "\nWhat's the item price ?\n>"
       @price = gets.chomp
       break if @price.length > 0 && @price.length <= 10
-      print "Price isn't correct, is it ?"
+      print "\nPrice isn't correct, is it ?"
     end
     return @price
   end
@@ -33,10 +35,10 @@ class View
   def get_quantity
     @quantity
     loop do
-      puts "How many of this item do you have in stock ?\n>"
+      print "\nHow many of this item do you have in stock ?\n>"
       @quantity = gets.chomp
       break if @quantity.length > 0 && @quantity.length <= 4
-      print "Isn't it a bit too many to hold ?"
+      print "\nIsn't it a bit excessive ?"
     end
     return @quantity
   end
@@ -44,10 +46,10 @@ class View
   def get_brand
     @brand
     loop do
-      puts "What's the item brand ?\n>"
+      print "\nWhat's the item brand ?\n>"
       @brand = gets.chomp
       break if @brand.length > 0 && @brand.length <= 30
-      print "Brand name should be between 1 and 30 characters. Please try again !"
+      print "\nBrand name should be between 1 and 30 characters. Please try again !"
     end
     return @brand
   end
@@ -55,21 +57,25 @@ class View
   def self.all(all_items)
     sorted_items = all_items.sort_by! { |i| i.name.downcase }
     sorted_items.each do |item|
-      print "#{item.id}, Item name : #{item.name}.\n"
+      print "-" * 50
+      print "\nID : #{item.id}, Item name : #{item.name}.\n"
     end
   end
 
   # def self.count(total)
-  #   puts "There are #{total} items."
+  #   print "There are #{total} items."
   # end
 
   def ask_item_id
-    puts "Which item would you like to pick ?\n>"
+    print "-" * 50
+    print "\nWhich item would you like to pick ?\n>"
     id = gets.chomp.to_i
     return id
   end
 
   def self.show(item)
-    print "Item ID : #{item.id}, Item name : #{item.name}, Item price : #{item.price}, Item quantity : #{item.quantity}, Item brand : #{item.brand}.\n"
+    print "-" * 50
+    print "\nItem ID : #{item.id}, Item name : #{item.name}, Item price : #{item.price}, Item quantity : #{item.quantity}, Item brand : #{item.brand}.\n"
+    print "-" * 50
   end
 end
--- lib/views/welcome_view.rb
+++ lib/views/welcome_view.rb
@@ -1,38 +1,48 @@
 class WelcomeView
   def self.get_input
-    puts "Whatcha wanna do ?"
-    puts "1. Browse Items."
-    puts "2. I wanna see details of one item !"
-    puts "3. Go to Admin Dashboard."
-    puts "4. Leave."
+    print "\n"
+    print "-" * 50
+    print "\nWhatcha wanna do ?\n"
+    print "\n1. Browse Items."
+    print "\n2. I wanna see details of one item !"
+    print "\n3. Go to Admin Dashboard."
+    print "\n4. Leave.\n>"
   end
 
   def self.get_admin_input
-    puts "Whatcha wanna do ?"
-    puts "1. I wanna create item !"
-    puts "2. I wanna browse items !"
-    puts "3. I wanna see details of one item !"
-    puts "4. I wanna delete a item !"
-    puts "5. I wanna leave."
+    print "\n"
+    print "-" * 50
+    print "\nWhatcha wanna do ?"
+    print "\n1. I wanna create item !"
+    print "\n2. I wanna browse items !"
+    print "\n3. I wanna see details of one item !"
+    print "\n4. I wanna delete a item !"
+    print "\n5. I wanna leave.\n>"
   end
 
   def self.error
-    puts "Invalid choice. Please focus and try again."
+    print "\nInvalid choice. Please focus and try again.\n"
   end
 
   def self.welcome
-    puts "Welcome to The Shop !"
+    print "*" * 50
+    print "\nWelcome to The Shop !\n"
+    print "*" * 50
   end
 
   def self.get_pwd
-    puts "Enter password : "
+    print "#" * 50
+    print "\nEnter password : "
   end
 
   def self.goodbye
-    puts "Good Bye !"
+    print "*" * 50
+    print "\nGood Bye !\n"
+    print "*" * 50
   end
 
   def self.ask_confirm
-    puts "Are you sure ? (y/n)"
+    print "#" * 50
+    print "\nAre you sure ? (y/n)\n"
   end
 end
