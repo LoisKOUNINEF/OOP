7eca53fb7d68b7031a4663cbfa9ff6520ff7e044
LoisKOUNINEF <lkouninef@gmail.com>
2022-06-09 16:23:29 +0200

    check input length on creation

--- lib/view.rb
+++ lib/view.rb
@@ -1,19 +1,55 @@
 class View
   def create_item
-    puts "What's the name ?"
-    print ">"
-    @name = gets.chomp
-    puts "Whats the price ?"
-    print ">"
-    @price = gets.chomp
-    puts "Whats the quantity ?"
-    print ">"
-    @quantity = gets.chomp
-    puts "Whats the brand ?"
-    print ">"
-    @brand = gets.chomp
+    get_name
+    get_price
+    get_quantity
+    get_brand
 
     return params = { name: @name, price: @price, quantity: @quantity, brand: @brand }
+  end
+
+  def get_name
+    @name
+    loop do
+      puts "What's the name ?\n>"
+      @name = gets.chomp
+      break if @name.length >= 0 and @name.length <= 100
+      print "error message! (please make this helpful) - try again "
+    end
+    return @name
+  end
+
+  def get_price
+    @price
+    loop do
+      puts "What's the price ?\n>"
+      @price = gets.chomp
+      break if @price.length >= 0 and @price.length <= 10
+      print "error message! (please make this helpful) - try again "
+    end
+    return @price
+  end
+
+  def get_quantity
+    @quantity
+    loop do
+      puts "What's the quantity ?\n>"
+      @quantity = gets.chomp
+      break if @quantity.length >= 0 and @quantity.length <= 4
+      print "error message! (please make this helpful) - try again "
+    end
+    return @quantity
+  end
+
+  def get_brand
+    @brand
+    loop do
+      puts "What's the brand ?\n>"
+      @brand = gets.chomp
+      break if @brand.length >= 0 and @brand.length <= 30
+      print "error message! (please make this helpful) - try again "
+    end
+    return @brand
   end
 
   def self.all(all_items)
