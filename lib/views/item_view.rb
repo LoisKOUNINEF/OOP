2499dc5a40fc8eb72b1ee6a93a8819db26a7b052
LoisKOUNINEF <lkouninef@gmail.com>
2022-06-11 17:21:21 +0200

    prevent crash when id doesnt exist

--- lib/views/item_view.rb
+++ lib/views/item_view.rb
@@ -74,8 +74,10 @@ class View
   end
 
   def self.show(item)
-    print "-" * 50
-    print "\nItem ID : #{item.id}, Item name : #{item.name}, Item price : #{item.price}, Item quantity : #{item.quantity}, Item brand : #{item.brand}.\n"
-    print "-" * 50
+    if item.nil?
+      print "-" * 50
+      print "\nItem ID : #{item.id}, Item name : #{item.name}, Item price : #{item.price}, Item quantity : #{item.quantity}, Item brand : #{item.brand}.\n"
+      print "-" * 50
+    end
   end
 end
